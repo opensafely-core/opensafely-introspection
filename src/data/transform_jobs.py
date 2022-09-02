a3092b35e9de3cf337169f79be821166475af8c8
Iain Dillingham <iain@dillingham.me.uk>
2022-09-02 11:59:14 +0100

    Transform status

--- src/data/transform_jobs.py
+++ src/data/transform_jobs.py
@@ -4,6 +4,13 @@ import click
 import pandas
 
 
+def transform_status(status):
+    status = status.str.lower()
+    status.loc[status == "0"] = pandas.NA
+    status = status.astype("category")
+    return status
+
+
 @click.command()
 @click.argument("jobs_in", type=pathlib.Path)
 @click.argument("jobs_out", type=pathlib.Path)
@@ -12,6 +19,7 @@ def main(jobs_in, jobs_out):
         jobs_in,
         parse_dates=["created_at", "started_at", "updated_at", "completed_at"],
     )
+    jobs.status = transform_status(jobs.status)
     jobs.to_feather(jobs_out)
 
 
