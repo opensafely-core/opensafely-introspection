b2633b836ac97b8bdabe4d6e725849adf198110f
Iain Dillingham <iain@dillingham.me.uk>
2022-09-02 11:32:47 +0100

    Parse dates

--- src/data/transform_jobs.py
+++ src/data/transform_jobs.py
@@ -8,7 +8,10 @@ import pandas
 @click.argument("jobs_in", type=pathlib.Path)
 @click.argument("jobs_out", type=pathlib.Path)
 def main(jobs_in, jobs_out):
-    jobs = pandas.read_csv(jobs_in)
+    jobs = pandas.read_csv(
+        jobs_in,
+        parse_dates=["created_at", "started_at", "updated_at", "completed_at"],
+    )
     jobs.to_feather(jobs_out)
 
 
